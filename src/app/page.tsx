d10fdd4c414f0ddfa5c28fde7498390008800b73
amnaduny <amnaduniakhara@gmail.com>
2025-08-22 12:46:05 +0700

    fix location map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import dynamic from "next/dynamic";
 
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
-import LocationMap from "./components/LocationMap";
+// Dynamically load LocationMap on the client only
+const LocationMap = dynamic(() => import("./components/LocationMap"), {
+  ssr: false,
+});
 import FollowUs from "./components/FollowUs";
 
 export default function Home() {
